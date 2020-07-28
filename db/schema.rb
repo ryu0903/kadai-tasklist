a414266b943ba0b753eb7bbab3cd54123efc1bff
ryutaro.higashi <xxx@gmail.com>
2020-07-28 11:05:37 +0000

    migrate re

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_102447) do
+ActiveRecord::Schema.define(version: 2020_07_28_110137) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.text "status"
   end
 
 end
