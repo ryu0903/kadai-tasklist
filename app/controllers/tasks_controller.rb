07e92a903d9d5c20705350a0ce61f5cf5f042401
ryutaro.higashi <xxx@gmail.com>
2020-08-11 09:29:32 +0000

    finish-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,7 @@
 class TasksController < ApplicationController
   include SessionsHelper
-  before_action :set_task, only: [:show, :edit, :update]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.all.page(params[:page]).per(10)
@@ -53,10 +52,6 @@ class TasksController < ApplicationController
   end
 
 private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
@@ -71,6 +66,7 @@ private
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
+      flash[:danger] = '別のユーザーによる登録です。'
       redirect_to root_url
     end
   end  
